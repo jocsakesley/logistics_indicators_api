f40c8f2cd488c981ba6cde31c0c1d3abb03bef3d
Jocsã Kesley <jocsakesley@MacBook-Air-de-Jocsa.local>
2025-01-17 09:00:10 -0300

    feat: handle customer_services features

--- src/repositories/customer_service_repository.py
+++ src/repositories/customer_service_repository.py
@@ -10,10 +10,19 @@ class CustomerServiceRepository(AbstractRepository):
     def __init__(self, db: DbSession):
         super().__init__()
         self.db = db.db
+        self.func = db.func
 
     def add(self, entity):
         try:
             self.db.add(entity)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            raise e
+    
+    def add_all(self, entities):
+        try:
+            self.db.add_all(entities)
             self.db.commit()
         except Exception as e:
             self.db.rollback()
@@ -28,11 +37,20 @@ class CustomerServiceRepository(AbstractRepository):
 
     def get_all(self):
         try:
-            customer_services = self.db.query(CustomerServiceModel).all()
+            customer_services = self.db.query(self.func.count(CustomerServiceModel.id)).scalar()
         except Exception as e:
-            raise e
+            self.db.rollback()
+            raise e  
         return customer_services
 
+    def filter(self, limit, offset, **kwargs):
+        try:
+            filtered_customer_services = self.db.query(CustomerServiceModel).filter_by(**kwargs).limit(limit).offset(offset).all()
+        except Exception as e:
+            self.db.rollback()
+            raise e
+        return filtered_customer_services
+    
     def update(self, customer_service_id, entity):
         try:
             updated = self.db.query(CustomerServiceModel).filter_by(id=customer_service_id).update(entity)
@@ -52,6 +70,11 @@ class ClientDoesNotExistException(Exception):
         super().__init__(self.message)
     
 class CustomerServiceDoesNotExistException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+class FilterClientIdException(Exception):
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
--- src/usecases/customer_services/get_all_customer_service_use_case.py
+++ src/usecases/customer_services/get_all_customer_service_use_case.py
@@ -1,10 +1,48 @@
 
+from src.repositories.customer_service_repository import FilterClientIdException
 from src.repositories.abstract_repository import AbstractRepository
-
+from flask import Request, url_for
 
 class GetAllCustomerServiceUseCase:
     def __init__(self, customer_service_repository: AbstractRepository):
         self.customer_service_repository = customer_service_repository
 
     def execute(self, *args, **kwargs):
-        return self.customer_service_repository.get_all()
+        request: Request = kwargs.get("request")
+        
+        dict_args = {**request.args}
+
+        limit = dict_args.pop("limit", "30")
+        offset = dict_args.pop("offset", "0")
+
+        if "total" in request.path.split("/"):
+            total = self.customer_service_repository.get_all()
+            return {"total": total}
+
+        # if not dict_args:
+        #     customer_services = self.customer_service_repository.get_all(limit, offset)
+        # else:
+        try:
+            customer_services = self.customer_service_repository.filter(limit, offset, **dict_args)
+        except Exception:
+            raise FilterClientIdException(f"Incorret values to parameters {list(dict_args.keys())}") 
+        
+        new_offset=str(int(offset) + int(limit))
+        data = {"atendimentos":[
+                        customer_service.to_dict() for customer_service in customer_services
+                        ],
+                "total_por_pagina": len(customer_services)}
+        if len(customer_services) >= int(limit):
+            data.update({"proxima_pagina": url_for('services.get_all_customer_services', 
+                                                limit=limit,
+                                                offset=new_offset,
+                                                **dict_args)})
+        return data
+            
+                 
+
+    
+
+
+
+
